e776bc66c3d617058ba0bc2de12f008c201d4c11
Aleksandr72rus <613086@mail.ru>
2025-08-21 12:23:40 +0500

    diplom1

--- fourth/devsearch/projects/views.py
+++ fourth/devsearch/projects/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Project
+from .forms import ProjectForm
 
 
 def projects(request):
@@ -16,4 +17,14 @@ def project(request, pk):
                   {'project': project_obj})
 
 
+def create_project(request):
+    form = ProjectForm()
 
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+        return redirect('projects')
+
+    return render(request, 'projects/form-template.html',
+                  {'form': form})
